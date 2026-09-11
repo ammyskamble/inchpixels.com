840f28f24f408a83a9b8e961815ea2e206cd1b43
ammyskamble <ammyskamble@users.noreply.github.com>
2026-09-11 15:10:53 +0530

    chore: generate wrangler temporary build files for development environment

---
+++ .wrangler/tmp/pages-r8bvxw/functionsRoutes-0.3802833508724027.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ___middleware_ts_onRequest } from "D:\\Testing\\Tools\\Inches to pixel\\inchpixels.com\\functions\\_middleware.ts"
+
+export const routes = [
+    {
+      routePath: "/",
+      mountPath: "/",
+      method: "",
+      middlewares: [___middleware_ts_onRequest],
+      modules: [],
+    },
+  ]
